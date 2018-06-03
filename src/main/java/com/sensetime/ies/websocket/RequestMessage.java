88a675666419b181933f76bb100277262d55ae8d
winer632@qq.com <winer632@qq.com>
2018-06-03 16:36:50 +0800

    init

---
+++ src/main/java/com/sensetime/ies/websocket/RequestMessage.java
@@ -0,0 +1,13 @@
+package com.sensetime.ies.websocket;
+
+/**
+ * Created by sang on 16-12-22.
+ */
+public class RequestMessage {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+}
